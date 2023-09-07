880d319e8370ab3e59f6489553ec2a561b1bae4a
kainy01 <kainykawai@gmail.com>
2023-09-07 14:28:09 +0300

    added desc

--- app/layout.js
+++ app/layout.js
@@ -7,20 +7,19 @@ const SourceCodePro = Source_Code_Pro({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Zainab Salah",
-  description: "Port ",
+  description:
+    "A passionate frontend developer originally from Iraq and currently based in Turkey. I hold a bachelor's degree in Mathematics, which has equipped me with a strong foundation for problem-solving and analytical thinking. ",
 };
 
 export default function RootLayout({ children }) {
   return (
-   
     <html lang="en">
-       <head>
+      <head>
         <link rel="icon" href="https://img.icons8.com/color/48/z.png" />
       </head>
       <body className={SourceCodePro.className} suppressHydrationWarning={true}>
         <Providers>{children}</Providers>
       </body>
     </html>
-
   );
 }
